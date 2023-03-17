1f60e37655dfc10f5b4549ba102882aca69d7ed0
bhendi-boi <bhendiisgreen@gmail.com>
2023-03-17 10:34:01 +0530

    finished exercise 08

--- src/08-function-return-type-annotations.problem.ts
+++ src/08-function-return-type-annotations.problem.ts
@@ -18,7 +18,22 @@ interface Post {
  * returns a user?
  */
 const makeUser = () => {
-  return {};
+  return {
+    id: 1,
+    firstName: "Matt",
+    lastName: "Pocock",
+    role: "admin",
+    posts: [
+      {
+        id: 1,
+        title: "How I eat so much cheese",
+      },
+      {
+        id: 2,
+        title: "Why I don't eat more vegetables",
+      },
+    ],
+  } as User;
 };
 
 it("Should return a valid user", () => {
